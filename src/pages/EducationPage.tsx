b8b2cebe1e6ecf4f153ce09ed6a8bb0759efe1bb
crizzdevs <abuancrisjulian1206@gmail.com>
2025-11-17 03:14:37 +0800

    Update EducationPage.tsx

--- src/pages/EducationPage.tsx
+++ src/pages/EducationPage.tsx
@@ -1,7 +1,9 @@
 import EducationSection from "@/components/sections/EducationSection";
+import { GameNav } from "@/components/GameNav"; // Adjust the import path based on where GameNav is located
 
 const EducationPage = () => (
-  <main>
+  <main className="pt-16"> {/* Add padding-top to account for the fixed nav height */}
+    <GameNav />
     <EducationSection />
   </main>
 );
