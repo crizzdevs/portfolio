a2f8da16d6c3d3cd5bb56be93eae621ef26f4677
crizzdevs <abuancrisjulian1206@gmail.com>
2025-11-17 02:03:19 +0800

    Create EducationPage component with EducationSection

---
+++ src/pages/EducationPage.tsx
@@ -0,0 +1,11 @@
+import EducationSection from "@/components/sections/EducationSection";
+
+const EducationPage: React.FC = () => {
+  return (
+    <main>
+      <EducationSection />
+    </main>
+  );
+};
+
+export default EducationPage;
