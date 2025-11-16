1303655fe496d783cd75c597cbf88089cd748e85
crizzdevs <abuancrisjulian1206@gmail.com>
2025-11-17 02:24:58 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EducationPage from "@/pages/EducationPage";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
@@ -16,8 +20,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="/education" element={<EducationPage />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
