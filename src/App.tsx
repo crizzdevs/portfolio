1578b91ab3d61862f351e7ec818b19b6407d6e0e
crizzdevs <abuancrisjulian1206@gmail.com>
2025-11-19 23:17:33 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/education" element={<EducationPage />} />
-          <Route path="/achievement" element={<AchievementPage />} />
+          <Route path="/achievements" element={<AchievementPage />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
