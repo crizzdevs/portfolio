d6246574e68a9843d751eeceaa0496cf87c57a18
crizzdevs <abuancrisjulian1206@gmail.com>
2025-11-19 22:59:38 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EducationPage from "@/pages/EducationPage";
-import AchievementPage from "@pages/AchievementPage";
+import AchievementPage from "@/pages/AchievementPage";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import "./index.css";
