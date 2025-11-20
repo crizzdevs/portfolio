cdf92c03a0fc7b1b35f40e1b314ce15b346d1836
crizzdevs <abuancrisjulian1206@gmail.com>
2025-11-20 22:04:40 +0800

    Update AchievementPage.tsx

--- src/pages/AchievementPage.tsx
+++ src/pages/AchievementPage.tsx
@@ -5,9 +5,21 @@ import { GameNav } from "@/components/GameNav";
 import { PixelCard } from "@/components/PixelCard.tsx";
 
 const curricular = [
-  "Ubbing iti Mallig - Internal Secretary (2023-2025)",
+  "Consistent Honors Student",
   "Editor in Chief - The Mega Flash (2023-2024)",
   "Barkada Kontra Bisyo - President (2024-2025)",
+  "Outstanding Performance - Social Sciences (2024-2025)".
+  "Outstanding Performance - Research (2024-2025)",
+  "Outstanding Performance - Work Immersion (2024-2025)",
+  "Leadership Award - Gold Medal (2024-2025)",
+  "Champion - School Based Editorial Writing (2022-2023)",
+  "Champion - School Based Editorial Writing (2023-2024)",
+  "Champion - School Based Editorial Writing (2024-2025)",
+  "Champion - Digital Poster Making (2024-2025)",
+  
+  
+  
+  
 ];
 
 const extraCurricular = [
